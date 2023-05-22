77c44e19948fcde7617918efcffe8a4c56de80f8
Khalid <shkhalid95@mail.ru>
2023-05-23 00:45:03 +0300

    refactor

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-import * as Grammy from 'https://deno.land/x/grammy@v1.15.3/mod.ts';
-import * as GrammyTypes from "https://deno.land/x/grammy_types@v3.0.3/mod.ts";
-import * as Axiod from 'https://deno.land/x/axiod@0.26.2/mod.ts';
-import { load } from "https://deno.land/std@0.188.0/dotenv/mod.ts";
+import * as Grammy from "https://deno.land/x/grammy@v1.16.1/mod.ts";
+import * as GrammyTypes from "https://deno.land/x/grammy_types@v3.1.1/mod.ts";
+import * as Axiod from "https://deno.land/x/axiod@0.26.2/mod.ts";
+import { load } from "https://deno.land/std@0.187.0/dotenv/mod.ts";
 
 export { Grammy, Axiod, GrammyTypes, load };
