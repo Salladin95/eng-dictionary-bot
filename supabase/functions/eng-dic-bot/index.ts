4f512cadb600157a14f10ffb9320accfe853c9c7
Khalid <shkhalid95@mail.ru>
2023-06-01 17:14:26 +0300

    refactor

--- supabase/functions/eng-dic-bot/index.ts
+++ supabase/functions/eng-dic-bot/index.ts
@@ -3,5 +3,10 @@ import { MyContext } from './contracts.ts';
 import { Bot, Context, serve, webhookCallback } from './deps.ts';
 import launchBot from './services/launchBot/launchBot.ts';
 
-const bot = new Bot<MyContext>(config.botToken || '');
-launchBot(bot);
+const token = config.botToken;
+if (token) {
+  const bot = new Bot<MyContext>(token);
+  launchBot(bot);
+} else {
+  console.warn("TOKEN IS EMPTY");
+}
